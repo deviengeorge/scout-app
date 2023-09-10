9767c4d2c0bf3a32c605835f00f2b4588a96ecb0
deviengeorge <deviengeorge99@gmail.com>
2023-09-10 08:28:05 +0300

    adding SEO

--- app/(full-page)/auth/layout.tsx
+++ app/(full-page)/auth/layout.tsx
@@ -5,10 +5,22 @@ import React from 'react';
 interface SimpleLayoutProps {
     children: React.ReactNode;
 }
-
 export const metadata: Metadata = {
-    title: 'PrimeReact Sakai',
-    description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.'
+    title: 'Nour ElAlam Website',
+    description: 'Nour ElAlam Scouts Website.',
+    robots: { index: false, follow: false },
+    viewport: { initialScale: 1, width: 'device-width' },
+    openGraph: {
+        type: 'website',
+        title: 'Nour ElAlam Website',
+        url: 'https://nour-elalam-scout.vercel.app/',
+        description: 'Nour ElAlam Scouts Website.',
+        images: ['https://nour-elalam-scout.vercel.app/scout-logo.png'],
+        ttl: 604800
+    },
+    icons: {
+        icon: '/favicon.ico'
+    }
 };
 
 export default function SimpleLayout({ children }: SimpleLayoutProps) {
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,6 +4,23 @@ import Layout from '../../layout/layout';
 interface AppLayoutProps {
     children: React.ReactNode;
 }
+export const metadata: Metadata = {
+    title: 'Nour ElAlam Website',
+    description: 'Nour ElAlam Scouts Website.',
+    robots: { index: false, follow: false },
+    viewport: { initialScale: 1, width: 'device-width' },
+    openGraph: {
+        type: 'website',
+        title: 'Nour ElAlam Website',
+        url: 'https://nour-elalam-scout.vercel.app/',
+        description: 'Nour ElAlam Scouts Website.',
+        images: ['https://nour-elalam-scout.vercel.app/scout-logo.png'],
+        ttl: 604800
+    },
+    icons: {
+        icon: '/favicon.ico'
+    }
+};
 
 export default function AppLayout({ children }: AppLayoutProps) {
     return <Layout>{children}</Layout>;
